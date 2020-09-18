39f92038e585032ef9c99cfc908951104a23582f
Maanas Arora <maanasarora23@gmail.com>
2020-09-18 14:32:19 -0400

    Updates

---
+++ precache-manifest.395e580f4944dee94e19d42851764d01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07aaf2ea06ca68a8080a68cac83ffe2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9510facd84074e517739",
+    "url": "./static/css/main.8ca526bf.chunk.css"
+  },
+  {
+    "revision": "c396f0560d6f3bd81d94",
+    "url": "./static/js/2.145149f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.145149f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9510facd84074e517739",
+    "url": "./static/js/main.af5e6cb5.chunk.js"
+  },
+  {
+    "revision": "92ee8d551ff3d9402230",
+    "url": "./static/js/runtime-main.79e601ff.js"
+  }
+]);
